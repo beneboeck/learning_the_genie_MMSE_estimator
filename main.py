ad6f0e57e427ff7f89d9c8d4242723c36018cc9a
beneboeck <63995517+beneboeck@users.noreply.github.com>
2022-07-19 16:40:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,21 +17,21 @@ import network_architecture_search as nas
 path = '/home/ga42kab/lrz-nashome/learning_the_genie_MMSE_estimator/data/'
 
 h_train = np.load(path + 'h_train.npy')
-h_test = np.save(path + 'h_test.npy')
-h_val = np.save(path + 'h_val.npy')
+h_test = np.load(path + 'h_test.npy')
+h_val = np.load(path + 'h_val.npy')
 
 
-angles_train = np.save(path + 'angles_train.npy')
-angles_test = np.save(path + 'angles_test.npy')
-angles_val = np.save(path + 'angles_val.npy')
+angles_train = np.load(path + 'angles_train.npy')
+angles_test = np.load(path + 'angles_test.npy')
+angles_val = np.load(path + 'angles_val.npy')
 
-gains_train = np.save(path + 'gains_train.npy')
-gains_test = np.save(path + 'gains_test.npy')
-gains_val = np.save(path + 'gains_val.npy')
+gains_train = np.load(path + 'gains_train.npy')
+gains_test = np.load(path + 'gains_test.npy')
+gains_val = np.load(path + 'gains_val.npy')
 
-C_sim_train = np.save(path + 'C_BS_train.npy')
-C_sim_test = np.save(path + 'C_BS_test.npy')
-C_sim_val = np.save(path + 'C_BS_val.npy')
+C_sim_train = np.load(path + 'C_BS_train.npy')
+C_sim_test = np.load(path + 'C_BS_test.npy')
+C_sim_val = np.load(path + 'C_BS_val.npy')
 
 
 lr,n_layers,n_conv,n_fully,kernel_size = nas.trail_1_NAS()
