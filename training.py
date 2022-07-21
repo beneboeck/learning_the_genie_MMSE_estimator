d45e29d2b454b39ba55f718feea1fb78afbf9fbe
beneboeck <63995517+beneboeck@users.noreply.github.com>
2022-07-21 17:48:14 +0200

    Update training.py

--- training.py
+++ training.py
@@ -20,7 +20,10 @@ def MMMSE_loss(h,C_learned,sig_n,y,device):
     R_inv = torch.linalg.solve_triangular(R,torch.eye(64).to(device),upper=True)
     Q_inv = Q.mH
     M_inv = R_inv @ Q_inv
-    H_hat = C_learned @ M_inv @ y
+    print(y.size())
+    H_hat = C_learned @ M_inv @ torch.transpose(y,dim0=1,dim1=2)
+    print(H_hat.size())
+    H_hat = torch.transpose(H_hat,dim0=1,dim1=2)
     loss = (torch.abs((H_hat - h)) ** 2).sum(dim=(1, 2)).mean()
     return loss
 
